7c1e850456ec87df7bd35ee4a65adf8b19352c3c
Ishu Bansal <ishubansal1400@gmail.com>
2020-04-19 01:11:08 +0530

    Update hqakFi.cpp

--- lexicographic_iterator/hqakFi.cpp
+++ lexicographic_iterator/hqakFi.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 using namespace std;
-void f(int i, string &s){
+void f(int i, string &s, int &k, bool virgin=true){
+	if(k<=0)	return;
 	if(!i){
 		cout<<s<<endl;
+		k--;
 		return;
 	}
+	char &x = s[s.size()-i];
 	for(char c='a'; c<'d'; c++){
-		s[s.size()-i] = c;
-		f(i-1,s);
+		if(virgin&&c<x)	continue;
+		x = c;
+		f(i-1,s,k,virgin);
+		virgin = false;
 	}
 }
 int main() {
-	int t,n;
+	int t,n,k;
 	string s;
 	cin>>t;
 	while(t--){
-		cin>>n;
+		cin>>s>>k;
+		n = s.size();
 		cout<<"n: "<<n<<endl;
-		s = string(n,'a');
-		f(n,s);
+		f(n,s,k);
 	}
 	return 0;
 }
